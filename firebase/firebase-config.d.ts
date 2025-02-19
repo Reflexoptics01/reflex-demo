2ba6fca8c7275d2ef9618460ac0997fa22472952
Reflexoptics01 <reflexopticsolutions@gmail.com>
2025-02-19 14:24:11 +0530

    Add files via upload

---
+++ firebase/firebase-config.d.ts
@@ -0,0 +1,4 @@
+declare module '../firebase/firebaseConfig' {
+  import { Firestore } from 'firebase/firestore';
+  export const db: Firestore;
+} 
---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import{getFirestore} from "@firebase/firestore"
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfsiT_0KoqBghS00b0_HuTivOFhr454Ho",
+  authDomain: "refles-optics-demo.firebaseapp.com",
+  projectId: "refles-optics-demo",
+  storageBucket: "refles-optics-demo.firebasestorage.app",
+  messagingSenderId: "696562114393",
+  appId: "1:696562114393:web:eda3d5ea3bd73937ee7648",
+  measurementId: "G-GNTBQC2ZPN"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app);
+export const auth =getAuth();
+export const storage=getStorage(app);
